42effc7158f0837e671915017776b282ebd5a35e
Kevin Ballard <kevin@sb.org>
2014-11-19 20:25:50 -0800

    Replace `make cargo-prep` with a real build script

--- Makefile
+++ Makefile
@@ -10,21 +10,4 @@ install: target/release/unicode
 
 clean:
 	cargo clean
-	-rm -f version
 .PHONY: clean
-
-ifeq ($(OUT_DIR),)
-cargo-prep:
-	$(error make cargo-prep must be called by cargo)
-.PHONY: cargo-prep
-else
-# the rest of the Makefile is only visible to Cargo
-
-cargo-prep: $(OUT_DIR)/version
-.PHONY: cargo-prep
-
-$(OUT_DIR)/version:
-	printf "%s (%s)\n" "$$(git describe --always --dirty)" "$$(date +'%F %T %z')" > $@
-.PHONY: $(OUT_DIR)/version
-
-endif
--- src/build.rs
+++ src/build.rs
@@ -1,9 +1,37 @@
-use std::io::Command;
-use std::io::process::{InheritFd,Ignored};
+extern crate time;
+
+use std::io::{Command,File};
+use std::io::process::{InheritFd,Ignored,ProcessOutput};
+use std::os;
 
 fn main() {
-    let mut cmd = Command::new("make");
-    cmd.arg("cargo-prep");
-    assert!(cmd.stdin(Ignored).stdout(InheritFd(1)).stderr(InheritFd(2))
-               .status().unwrap().success());
+    let version = git_describe();
+    let timestamp = timestamp();
+
+    let dst = Path::new(os::getenv("OUT_DIR").unwrap());
+    let mut f = File::create(&dst.join("version")).unwrap();
+    (writeln!(f, "{} ({})", version, timestamp)).unwrap();
+}
+
+fn git_describe() -> String {
+    let mut cmd = Command::new("git");
+    cmd.arg("describe").arg("--always").arg("--dirty");
+    let output = run(cmd);
+    assert!(output.status.success());
+    let mut output = String::from_utf8(output.output).unwrap();
+    while output.ends_with("\n") {
+        let len = output.len()-1;
+        output.truncate(len);
+    }
+    assert!(!output.is_empty());
+    output
+}
+
+fn timestamp() -> String {
+    time::strftime("%F %T %z", &time::now()).unwrap()
+}
+
+fn run(mut cmd: Command) -> ProcessOutput {
+    println!("running {}", cmd);
+    cmd.stdin(Ignored).stderr(InheritFd(2)).output().unwrap()
 }
