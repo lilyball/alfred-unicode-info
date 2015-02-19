10b2c0d54b348148983c01cef0264e03e7a25b69
Kevin Ballard <kevin@sb.org>
2015-02-18 23:25:52 -0800

    Convert build script to the new path/process/fs modules

--- src/build.rs
+++ src/build.rs
@@ -1,29 +1,28 @@
-#![feature(io,env,path,std_misc)]
+#![feature(fs,io,env,os,path,process,std_misc)]
 
-use std::old_io::{Command,File};
-use std::old_io::process::{InheritFd,Ignored,ProcessOutput};
 use std::env;
-use std::os::unix::OsStringExt;
+use std::ffi::OsString;
+use std::fs::File;
+use std::io::Write;
+use std::os::unix::prelude::*;
+use std::path::PathBuf;
+use std::process::{self,Command,Stdio};
 
 fn main() {
     let version = git_describe();
     let timestamp = timestamp();
 
-    let dst = Path::new(env::var_os("OUT_DIR").expect("Missing environment variable OUT_DIR").into_vec());
-    let mut f = File::create(&dst.join("version")).unwrap();
+    let out_dir = env::var_os("OUT_DIR").expect("Missing environment variable OUT_DIR").into_vec();
+    let mut dst = PathBuf::new(&<OsString as OsStringExt>::from_vec(out_dir));
+    dst.push("version");
+    let mut f = File::create(&dst).unwrap();
     (writeln!(&mut f, "{} ({})", version, timestamp)).unwrap();
 }
 
 fn git_describe() -> String {
-    let mut cmd = Command::new("git");
-    cmd.arg("describe").arg("--always").arg("--dirty");
-    let output = run(&mut cmd);
+    let output = run(Command::new("git").args(&["describe", "--always", "--dirty"]));
     assert!(output.status.success());
-    let mut output = String::from_utf8(output.output).unwrap();
-    while output.ends_with("\n") {
-        let len = output.len()-1;
-        output.truncate(len);
-    }
+    let output = chomp(String::from_utf8(output.stdout).unwrap());
     assert!(!output.is_empty());
     output
 }
@@ -31,12 +30,12 @@ fn git_describe() -> String {
 fn timestamp() -> String {
     let output = run(Command::new("date").arg("+%F %T %z"));
     assert!(output.status.success());
-    chomp(String::from_utf8(output.output).unwrap())
+    chomp(String::from_utf8(output.stdout).unwrap())
 }
 
-fn run(cmd: &mut Command) -> ProcessOutput {
+fn run(cmd: &mut Command) -> process::Output {
     println!("running {:?}", cmd);
-    cmd.stdin(Ignored).stderr(InheritFd(2)).output().unwrap()
+    cmd.stdin(Stdio::null()).stderr(Stdio::inherit()).output().unwrap()
 }
 
 fn chomp(mut s: String) -> String {
